0da153718aafdf8ea38b5330dc8df6d45f810750
vaibhav-bhoir <66204741+vaibhav-bhoir@users.noreply.github.com>
2021-01-07 22:50:00 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-// dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
 
-dbPassword = "mongodb+srv://vaibhav123:Vaibhav123@cluster0.j3p6p.mongodb.net/test?retryWrites=true&w=majority";
+dbPassword = "mongodb+srv://YOUR_USERNAME:YOURPASSWORD@cluster0.j3p6p.mongodb.net/test?retryWrites=true&w=majority";
 
 
 
